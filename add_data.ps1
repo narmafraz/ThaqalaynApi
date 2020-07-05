0d557e86d25162d09dcb41be8f76b67ee0dad10f
Sadegh <narmafraz@gmail.com>
2020-07-05 01:47:21 +0100

    Remove env variables not required

--- add_data.ps1
+++ add_data.ps1
@@ -1,8 +1,5 @@
 $Env:PYTHONPATH = $PSScriptRoot
 $Env:BACKEND_CORS_ORIGINS = "http://localhost, http://localhost:4200, http://localhost:3000, http://localhost:8080, https://localhost, https://localhost:4200, https://localhost:3000, https://localhost:8080, http://dev.thaqalayn.com, https://stag.thaqalayn.com, https://thaqalayn.com, http://local.dockertoolbox.tiangolo.com, http://localhost.tiangolo.com"
 $Env:PROJECT_NAME = "Thaqalayn"
-$Env:SECRET_KEY = "changethis_secret_key"
-$Env:FIRST_SUPERUSER = "admin@thaqalayn.com"
-$Env:FIRST_SUPERUSER_PASSWORD = "changethis_first_superuser_password"
 
 python .\data\main_add.py
